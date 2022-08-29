c1f4c4e7e5d3efd6c9cb56f50c6adad91832dd2e
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-08-30 01:10:11 +0530

    new

--- 73Set_Matrix_Zeroes.cpp
+++ 73Set_Matrix_Zeroes.cpp
@@ -40,19 +40,12 @@ void setZeroesApproach1(vector<vector<int>>& matrix) {
 
 void setZeroesApproach2(vector<vector<int>>& matrix) {
     int row1 = 1;
-    int col1 = 1;
     for(int i = 0; i < matrix.size(); i++) {
         for(int j = 0; j < matrix[0].size(); j++) {
             if(matrix[i][j] == 0) {
-                if(j == 0 && i != 0) {
-                    col1 = 0;
-                    matrix[i][0] = 0;
-                } else if(i == 0 && j != 0) {
+                if(i == 0) {
                     row1 = 0;
-                    matrix[0][j] = 0;
-                } else if(i == 0 && j == 0) {
-                    row1 = 0;
-                    col1= 0;
+                    matrix[i][j] = 0;
                 } else {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
@@ -65,9 +58,7 @@ void setZeroesApproach2(vector<vector<int>>& matrix) {
         for(int j = matrix[0].size()-1; j >= 0; j--) {
             if(i == 0 && row1 == 0) {
                 matrix[i][j] = 0;
-            } else if(j == 0 && col1 == 0) {
-                matrix[i][j] = 0;
-            } else if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+            } else if(i != 0 && (matrix[i][0] == 0 || matrix[0][j] == 0)) {
                 matrix[i][j] = 0;
             }
         }
